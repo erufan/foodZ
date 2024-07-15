76b10076f8785af4fb11ffe34125bb82ed312c42
erfan taheri <erfanta197@gmail.com>
2024-07-16 00:17:52 +0330

    style: Enhance appearance of Card component

--- src/components/main/FoodCard.tsx
+++ src/components/main/FoodCard.tsx
@@ -5,18 +5,32 @@ import {
   Heading,
   Image,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 
 const FoodCard = ({ image, title }: { image: string; title: string }) => {
   return (
     <>
-      <Card>
+      <Card display="flex" justifyItems="end">
         <CardHeader>
-          <Image src={image} borderRadius="50%" width="250px" height="250px" />
+          <Image
+            src={image}
+            borderRadius="50%"
+            width="150px"
+            height="150px"
+            margin="auto"
+            alt={title}
+          />
+          <Heading fontSize="large" textAlign="center" marginTop={4}>
+            {title}
+          </Heading>
         </CardHeader>
         <CardBody>
-          <Heading>{title}</Heading>
-          <Text>Lorem ipsum dolor sit amet consectetur adipisicing ...</Text>
+          <VStack height="100%" justify="end">
+            <Text align="center">
+              Lorem ipsum dolor sit amet consectetur adipisicing ...
+            </Text>
+          </VStack>
         </CardBody>
       </Card>
     </>
