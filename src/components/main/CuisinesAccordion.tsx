b24a15c0da0e9fee13f6cb097d38903c9c876013
erfan taheri <erfanta197@gmail.com>
2024-07-17 00:12:17 +0330

    feat: Implement cuisine selection with dynamic parameters

--- src/components/main/CuisinesAccordion.tsx
+++ src/components/main/CuisinesAccordion.tsx
@@ -1,20 +1,75 @@
 import Accordion from "../ui/Accordion";
 import { CheckboxGroup, Stack, Checkbox } from "@chakra-ui/react";
 import Cuisines from "../../data/Cuisines";
+import { useSearchParams } from "react-router-dom";
 
 const CuisinesAccordion = () => {
+  const [params, setParams] = useSearchParams();
+
+  const cuisinesHandler = (c: { name: string }) => {
+    const thereISNoCuisines = function () {
+      return !params.get("Cuisines");
+    };
+    const cuisinesHasTheregion = function () {
+      return params.get("Cuisines")?.search(c.name) !== -1;
+    };
+    const addTheRegion = function () {
+      return setParams({
+        ...params,
+        Cuisines: params.get("Cuisines") + "," + c.name,
+      });
+    };
+    const isFirstTheRegion = function () {
+      return params.get("Cuisines")?.indexOf(c.name) == 0;
+    };
+    const isAloneRegion = function () {
+      return params.get("Cuisines")?.search(",") == -1;
+    };
+
+    const remove = function (type: "first" | "notFirst" | "removeCuisines") {
+      if (type === "first")
+        return setParams({
+          ...params,
+          Cuisines: params.get("Cuisines")?.replace(c.name + ",", ""),
+        });
+      if (type === "notFirst")
+        return setParams({
+          ...params,
+          Cuisines: params.get("Cuisines")?.replace("," + c.name, ""),
+        });
+      if (type === "removeCuisines")
+        return setParams({
+          ...params,
+        });
+    };
+
+    if (thereISNoCuisines()) {
+      return setParams({ ...params, Cuisines: c.name });
+    } else {
+      if (cuisinesHasTheregion()) {
+        isFirstTheRegion()
+          ? isAloneRegion()
+            ? remove("removeCuisines")
+            : remove("first")
+          : remove("notFirst");
+      } else {
+        addTheRegion();
+      }
+    }
+  };
+
   return (
     <Accordion buttonText="Cuisines" height="45vh">
       <CheckboxGroup colorScheme="green">
         <Stack>
-          {Cuisines.map((C) => (
+          {Cuisines.map((c) => (
             <Checkbox
               inlineSize={1}
-              value={C.name}
-              onClick={() => console.log("hi")}
-              key={C.name}
+              value={c.name}
+              key={c.name}
+              onChange={() => cuisinesHandler(c)}
             >
-              {C.name}
+              {c.name}
             </Checkbox>
           ))}
         </Stack>
