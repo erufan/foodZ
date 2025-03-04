50df190945923191065fe033959469e6e35bf8f0
erfan taheri <erfanta197@gmail.com>
2025-03-04 18:00:17 +0330

    feat: normalize css for all browser

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import muiTheme from "./muiconfig.ts";
 
 const queryClient = new QueryClient();
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
         <RouterProvider router={router} />
         <ReactQueryDevtools />
       </ThemeProvider>
