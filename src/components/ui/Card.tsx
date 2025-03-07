537d64cb87a47746a0f06b3abb1bfb4ab5a57037
erfan taheri <erfanta197@gmail.com>
2025-03-07 19:18:22 +0330

    refactor: Replace Box components with Stack

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,6 +1,7 @@
 import Badge from "@mui/material/Badge/Badge";
 import Box from "@mui/material/Box/Box";
 import Paper from "@mui/material/Paper/Paper";
+import Stack from "@mui/material/Stack";
 
 import RestaurantIcon from "@mui/icons-material/Restaurant";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -46,18 +47,19 @@ const Card = () => {
         </Typography>
       </Box>
       <Box sx={{ marginTop: "1.4rem" }}>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Stack direction="row" sx={{ alignItems: "center" }}>
+          <Stack direction="row" sx={{ alignItems: "center" }}>
             <TimerIcon />
             <Typography variant="subtitle1">30 Minutes</Typography>
-          </Box>
-          <Box
-            sx={{ display: "flex", alignItems: "center", marginLeft: "1.5rem" }}
+          </Stack>
+          <Stack
+            direction="row"
+            sx={{ alignItems: "center", marginLeft: "1.5rem" }}
           >
             <RestaurantIcon />
             <Typography variant="subtitle1">snack</Typography>
-          </Box>
-        </Box>
+          </Stack>
+        </Stack>
       </Box>
     </Paper>
   );
